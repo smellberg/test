0c7cb3c9dbc83b90be921b89460b24758001e9f8
smellberg <smellberg@gmail.com>
2023-09-05 14:29:25 +0200

    test

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
 
+package org.sandra;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        final double celsius = 25; //Grader celsius, en låst siffra
+        double fahrenheit = (celsius)/1.8+32;
+
+        System.out.println(celsius + " grader celsius är lika med: " + fahrenheit + " fahrenheit");
+
+    }
+}
